96ef9a951127521d20e7a7d43917fd44ec69dda2
Felipe Ribeiro <piporibeiro@gmail.com>
2022-12-14 16:22:43 -0300

    test: Send Notification

---
+++ src/application/use-cases/send-notification.spec.ts
@@ -0,0 +1,19 @@
+import { SendNotification } from './send-notification';
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
+
+describe('Send Notification', () => {
+  it('should be able to send a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const sendNotification = new SendNotification(notificationsRepository);
+
+    const { notification } = await sendNotification.execute({
+      category: 'social',
+      content: 'any content',
+      recipientId: '12345',
+    });
+
+    expect(notificationsRepository.notifications).toBeTruthy();
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
+  });
+});
